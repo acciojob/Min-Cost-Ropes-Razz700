9c778ef79565959c0fbc3c93d22684db24204840
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-05 17:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function mincost(arr)
 { 
 //write your code here
+		function sort(arr){arr.sort((a,b)=>{return a-b;});}
+	while (arr.length>1){
+	    sort(arr);
+	   
+		cost=arr[0]+arr[1];
+		sum=sum+cost;
+	arr.shift();
+	arr.shift();
+	arr.unshift(cost);
+}
+
+	}
+
 // return the min cost
+console.log(sum);
   
 }
 
